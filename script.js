8369db1192201918c2205aff1481336cafe63202
Jonathan Dixon <jdixon34@cnm.edu>
2022-05-31 10:43:37 -0600

    added script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// In pseudocode, write a function that takes an array of numbers, and returns another array that counts the number of times each number appears in the array.
+//
+// In pseudocode, write a function that takes two integers. For each integer in between those two numbers (inclusive), output 'fizz' if the number is even, 'buzz' if the number is divisible by 3, and 'baz' without 'fizz' or 'buzz' if the number is divisible by both.
+//
+// Given this css, draw a box model of the div.
+// div {
+//     width: 250px;
+//     height: 110px;
+//     margin-top: 10px;
+//     padding: 20px;
+// }
+
+// const array of numbers = array
+// const new array = newArray
+// indexCounter = 0
+// numberCounter = 1
+
+// loop until indexCounter = length of array
+    // loop until index = length of array - 1
+        // if array[i] = array[i + 1]
+            // numberCounter = numberCounter + 1
+            // i = i + 1
+        // else
+            // i = i + 1
+        // return numberCounter
+    // add numberCounter value to the newArray
+    // indexCounter = indexCounter + 1
+// return newArray
+
+// const integer 1 = integer1
+// const integer 2 = integer2
+//
