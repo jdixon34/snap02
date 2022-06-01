838804f7ec5164a0ff79a3f2086c3d52ad73aef7
Jonathan Dixon <jdixon34@cnm.edu>
2022-06-01 09:44:18 -0600

    6-1-2022 code along/snap challenge

---
+++ script2.js
@@ -0,0 +1,37 @@
+// function returnVowels(String1) {
+//      convert String to array1
+//      const newArray = ''
+//      const newString = ''
+//      for each element in array1 do this
+//          if index[0] is not equal to 'a, e, i, o or u'
+//              add/insert string into index position of newArray
+//              add 1 to index
+//          else
+//              add 1 to index
+//          return newArray
+//      convert newArray to string in newString
+//      return newString
+// }
+
+
+
+// declare variable greatestNumber
+// declare variable indexPosition set to 0
+//
+// loop until index position is 1 less than length of array
+//     if index[indexPosition] >= index[indexPosition + 1]
+//         insert value of indexPosition into variable greatestNumber
+//     else
+//         insert value of (indexPosition + 1) into variable greatestNumber
+// return greatestNumber
+
+
+// declare fibArray as an array of [0, 1]
+// declare indexCounter as 0
+// declare newNumber as 0
+//
+// loop until length of fibArray === 100
+//     newNumber = index[indexCounter] + index[indexCounter + 1]
+//     add newNumber value at the end of fibArray
+//     indexCounter + 1
+// return fibArray
